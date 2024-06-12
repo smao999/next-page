2b66771c8da84119ea03996a6f49263fd7085b7b
v_xiaqingsong <v_xiaqingsong01@baidu.com>
2024-06-12 19:11:30 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { inter } from "./ui/fonts";
 import "./globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/video/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const PageVideo: FC = () => {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <p>video</p>
+        </main>
+    )
+}
+
+export default PageVideo;
