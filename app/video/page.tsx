d1b0f6b90f53490ab1f1919f9170d46112d8bd47
v_xiaqingsong <v_xiaqingsong01@baidu.com>
2024-06-14 19:33:20 +0800

    视频解码

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FC, useState } from "react";
-// import {MP4Clip} from '@webav/av-cliper';
+import {MP4Clip} from '@webav/av-cliper';
 
 import {Button, Radio, Space} from 'antd';
 
@@ -27,64 +27,64 @@ const PageVideo: FC = () => {
     const [ctx, setCtx] = useState<CanvasRenderingContext2D | null | undefined>(null);
 
     const start = async (speed:number, videoType: keyof typeof videos, ctx: CanvasRenderingContext2D) => {
-        // const resp1 = await fetch(videos[videoType]);
-        // const clip = new MP4Clip(resp1.body!);
-        // await clip.ready;
-        // timesSpeedDecode(speed, clip, ctx);
+        const resp1 = await fetch(videos[videoType]);
+        const clip = new MP4Clip(resp1.body!);
+        await clip.ready;
+        timesSpeedDecode(speed, clip, ctx);
     }
 
-    // const timesSpeedDecode = async (times: number, clip: MP4Clip, ctx: CanvasRenderingContext2D) => {
-    //     if(times === Infinity) {
-    //         let time = 0;
-    //         while(true) {
-    //             const {state, video} = await clip.tick(time);
-    //             if(state === 'done') break;
-    //             if(state === 'success' && video) {
-    //                 ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    //                 ctx.drawImage(
-    //                     video, 
-    //                     0,
-    //                     0,
-    //                     video.codedWidth,
-    //                     video.codedHeight, 
-    //                     0,
-    //                     0,
-    //                     ctx.canvas.width,
-    //                     ctx.canvas.height
-    //                 );
-    //                 video.close();
-    //             };
-    //             time += 33000;
-    //         }
-    //         clip.destroy();
-    //     } else {
-    //         let startTime = performance.now();
-    //         const timer = setInterval(async () => {
-    //             const {state, video} = await clip.tick(
-    //                 Math.round((performance.now() - startTime) * 1000) * times
-    //             );
-    //             if(state === 'done') {
-    //                 clearInterval(timer);
-    //                 clip.destroy();
-    //                 return;
-    //             }
-    //             if(state === 'success' && video) {
-    //                 ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    //                 ctx.drawImage(
-    //                     video, 
-    //                     0,
-    //                     0,
-    //                     video.codedWidth,
-    //                     video.codedHeight, 
-    //                     0,
-    //                     0,
-    //                     ctx.canvas.width,
-    //                     ctx.canvas.height
-    //                 );
-    //             }
-    //         }, 1000 / 30)
-    //     }
-    // }
+    const timesSpeedDecode = async (times: number, clip: MP4Clip, ctx: CanvasRenderingContext2D) => {
+        if(times === Infinity) {
+            let time = 0;
+            while(true) {
+                const {state, video} = await clip.tick(time);
+                if(state === 'done') break;
+                if(state === 'success' && video) {
+                    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                    ctx.drawImage(
+                        video, 
+                        0,
+                        0,
+                        video.codedWidth,
+                        video.codedHeight, 
+                        0,
+                        0,
+                        ctx.canvas.width,
+                        ctx.canvas.height
+                    );
+                    video.close();
+                };
+                time += 33000;
+            }
+            clip.destroy();
+        } else {
+            let startTime = performance.now();
+            const timer = setInterval(async () => {
+                const {state, video} = await clip.tick(
+                    Math.round((performance.now() - startTime) * 1000) * times
+                );
+                if(state === 'done') {
+                    clearInterval(timer);
+                    clip.destroy();
+                    return;
+                }
+                if(state === 'success' && video) {
+                    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                    ctx.drawImage(
+                        video, 
+                        0,
+                        0,
+                        video.codedWidth,
+                        video.codedHeight, 
+                        0,
+                        0,
+                        ctx.canvas.width,
+                        ctx.canvas.height
+                    );
+                }
+            }, 1000 / 30)
+        }
+    }
 
     return(
         <main className="flex min-h-screen flex-col items-center gap-6 justify-start p-24">
